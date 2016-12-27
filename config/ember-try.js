edd0e5ea0d5af99fdc2aabf5b50aa9d401e93220
simonihmig <ihmig@kaliber5.de>
2016-12-27 17:58:59 +0100

    updated ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,3 +1,54 @@
 /*jshint node:true*/
 module.exports = {
+  scenarios: [
+    {
+      name: 'ember-2.4-lts',
+      bower: {
+        dependencies: {
+          "ember": "~2.4.0"
+        }
+      }
+    },
+    {
+      name: 'ember-2.8-lts',
+      bower: {
+        dependencies: {
+          "ember": "~2.8.0"
+        }
+      }
+    },
+    {
+      name: 'ember-release',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
+      }
+    },
+    {
+      name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      }
+    },
+    {
+      name: 'ember-canary',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
+      }
+    }
+  ]
 };
