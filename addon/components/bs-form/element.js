7dc8a31f5a3bfb93805d5e789da00df8d4408ac5
simonihmig <ihmig@kaliber5.de>
2017-03-17 10:20:27 +0100

    Explicitly set hasValidator to support https://github.com/kaliber5/ember-bootstrap/pull/266

--- addon/components/bs-form/element.js
+++ addon/components/bs-form/element.js
@@ -4,6 +4,9 @@ import BsFormElement from 'ember-bootstrap/components/bs-form/element';
 const { computed, defineProperty, A } = Ember;
 
 export default BsFormElement.extend({
+
+  hasValidator: computed.notEmpty('model.validate'),
+
   setupValidations() {
     let key = `model.error.${this.get('property')}.validation`;
     defineProperty(this, 'errors', computed(`${key}[]`, function() {
