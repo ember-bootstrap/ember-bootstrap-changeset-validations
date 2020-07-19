47a7fe00c3845e6599f2e66a09fe3003b2caccde
Jeldrik Hanschke <admin@jhanschke.de>
2020-07-19 16:50:23 +0200

    remove computed property and refactor to native class

--- addon/components/bs-form/element.js
+++ addon/components/bs-form/element.js
@@ -1,24 +1,15 @@
-import { notEmpty } from '@ember/object/computed';
-import { defineProperty, computed } from '@ember/object';
-import { A } from '@ember/array';
 import BsFormElement from 'ember-bootstrap/components/bs-form/element';
 
-export default BsFormElement.extend({
-  '__ember-bootstrap_subclass' : true,
+export default class BsFormElementWithChangesetValidationsSupport extends BsFormElement {
+  '__ember-bootstrap_subclass' = true;
 
-  hasValidator: notEmpty('model.validate'),
-
-  setupValidations() {
-    // `Changeset.error` is a getter based on a tracked property. Since it's a
-    // derived state it's not working together with computed properties smoothly.
-    // As a work-a-round we observe the `Changeset._errors` computed property
-    // directly, which holds the state. This is not optimal cause it's private.
-    // Should refactor to native getter as soon as `<FormElement>` component
-    // of Ember Bootstrap supports native getters for `FormElement.errors`
-    // property.
-    let key = `model.error.${this.get('property')}.validation`;
-    defineProperty(this, 'errors', computed(`model._errors`, function() {
-      return A(this.get(key));
-    }));
+  get errors() {
+    return [
+      this.model.error[this.property]?.validation
+    ];
   }
-});
+
+  get hasValidator() {
+    return typeof this.model.validate === 'function';
+  }
+}
