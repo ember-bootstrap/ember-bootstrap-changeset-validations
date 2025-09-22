7c30e14d4b3583789ad5aacf4d6b23de823f5aa5
Jeldrik Hanschke <admin@jhanschke.de>
2025-09-23 00:23:56 +0200

    ensure test coverage with Ember 4.12 and 5.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -24,6 +24,22 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.12.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
