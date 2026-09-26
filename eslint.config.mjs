ca5cd915860fcea13d1af10748ac22542c9c33f8
Jeldrik Hanschke <admin@jhanschke.de>
2026-09-26 19:18:41 +0200

    fix warp drive left-over

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,6 @@ import globals from 'globals';
 import js from '@eslint/js';
 
 import ember from 'eslint-plugin-ember/recommended';
-import WarpDrive from 'eslint-plugin-warp-drive/recommended';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import qunit from 'eslint-plugin-qunit';
 import n from 'eslint-plugin-n';
@@ -39,7 +38,6 @@ export default [
   eslintConfigPrettier,
   ember.configs.base,
   ember.configs.gjs,
-  ...WarpDrive,
   /**
    * Ignores must be in their own object
    * https://eslint.org/docs/latest/use/configure/ignore
